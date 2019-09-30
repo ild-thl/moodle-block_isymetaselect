4368bf96da06ba7f8f1cf75530454557a28bea46
oncampus <education@oncampus.de>
2019-09-30 12:26:47 +0200

    Missing english translations

--- lang/en/block_ildmetaselect.php
+++ lang/en/block_ildmetaselect.php
@@ -2,23 +2,23 @@
 
 $string['pluginname'] = 'Course Overview';
 
-$string['proctime_name'] = 'Bearbeitungszeit';
-$string['proctime_all'] = 'Alle Bearbeitungszeiten';
-$string['proctime_upto15'] = 'Bis 15 Stunden';
-$string['proctime_between16and20'] = '16 - 20 Stunden';
-$string['proctime_between21and25'] = '21 - 25 Stunden';
-$string['proctime_morethan25'] = 'Über 25 Stunden';
+$string['proctime_name'] = 'Workload';
+$string['proctime_all'] = 'All  Workloa';
+$string['proctime_upto15'] = 'Up to 15 Hours';
+$string['proctime_between16and20'] = '16 - 20 Hours';
+$string['proctime_between21and25'] = '21 - 25 Hours';
+$string['proctime_morethan25'] = 'Over 25 Hours';
 
 $string['starttime_name'] = 'Course Start';
-$string['starttime_all'] = 'All Coursestarts';
+$string['starttime_all'] = 'All Course Starts';
 $string['starttime_current'] = 'Currently Running';
 $string['starttime_less2weeks'] = 'In the next 2 weeks starting';
 $string['starttime_between3and4weeks'] = 'In 3-4 weeks';
 $string['starttime_between5and6weeks'] = 'In 5-6 weeks';
 $string['starttime_between7and8weeks'] = 'In 7-8 weeks';
 
-$string['courselanguage'] = 'Courselanguage';
-$string['courselanguage_all'] = 'All Courselanguages';
+$string['courselanguage'] = 'Course Language';
+$string['courselanguage_all'] = 'All Course Languages';
 
 $string['university'] = 'University';
 $string['university_all'] = 'All Universities';
