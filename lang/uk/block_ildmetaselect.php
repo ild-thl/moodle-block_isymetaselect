3c532df78f4e91f59ce00250de7b50e97b6efe3c
Tom <thomas.muschal@th-luebeck.de>
2025-02-12 13:30:14 +0100

    First translations added

---
+++ lang/uk/block_ildmetaselect.php
@@ -0,0 +1,93 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Strings for component 'blocks_ildmetaselect', language 'uk'
+ *
+ * @package    blocks_ildmetaselect
+ * @category   string
+ * @copyright  2023 Institut für digitale Systeme <dev.ild@th-luebeck.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'ISy Course Overview';
+
+$string['blocktitle'] = 'Kursübersicht';
+
+$string['proctime_name'] = 'Bearbeitungszeit';
+$string['proctime_all'] = 'Alle Bearbeitungszeiten';
+$string['proctime_upto15'] = 'Bis 15 {$a}';
+$string['proctime_between16and20'] = '16 - 20 {$a}';
+$string['proctime_between21and25'] = '21 - 25 {$a}';
+$string['proctime_morethan25'] = 'Über 25 {$a}';
+
+$string['starttime_name'] = 'Kursbeginn';
+$string['starttime_all'] = 'Alle Kursbeginne';
+$string['starttime_current'] = 'Aktuell laufend';
+$string['starttime_less2weeks'] = 'Innerhalb der nächsten 2 Wochen';
+$string['starttime_between3and4weeks'] = 'In 3-4 Wochen';
+$string['starttime_between5and6weeks'] = 'In 5-6 Wochen';
+$string['starttime_between7and8weeks'] = 'In 7-8 Wochen';
+
+$string['courselanguage'] = 'Мова курсу';
+$string['courselanguage_all'] = 'Alle Kurssprachen';
+
+$string['provider'] = 'Institution';
+$string['provider_all'] = 'Alle Institutionen';
+
+$string['subjectarea'] = 'Галузь знань';
+$string['subjectarea_all'] = 'Alle Wissensgebiete';
+
+$string['lecturer_detail'] = 'Автори / авторки';
+$string['provider_detail'] = 'Anbietende Hochschule';
+$string['courselanguage_detail'] = 'Мова курсу';
+$string['subjectarea_detail'] = 'Галузь знань';
+$string['avgworkload_detail'] = 'Середнє навантаження';
+$string['starttime_detail'] = 'Kursbeginn';
+
+$string['hours'] = 'Stunden';
+$string['free'] = 'Kostenfrei';
+$string['enrol'] = 'Einschreiben';
+$string['tocourse'] = 'zum Kurs';
+
+$string['metashare'] = 'teilen';
+$string['metatweet'] = 'tweeten';
+
+$string['noresultsfound'] = 'Keine Ergebnisse gefunden.';
+
+$string['h_awaits'] = 'Що очікує Вас на даному курсі?';
+$string['h_learn'] = 'Чого можна навчитися на цьому курсі?';
+$string['h_outline'] = 'Структура';
+$string['h_further_auth'] = 'Автори / авторки';
+$string['h_target_group'] = 'Цільова група';
+$string['h_confirmation'] = 'Teilnahmebestätigung';
+$string['license'] = 'Ліцензія';
+
+$string['filter'] = 'Filtern';
+// ADDED tinjohn 20220920
+$string['add_detail_page'] = 'Kursdetailseite anzeigen';
+$string['add_detail_page_help'] = 'Die Kursdetailseite zeigt weitere Inhaltsangaben zum Kurs und stellt einen zusätzlichen Schritt bis zur Einschreibung dar. Dieser Schritt kann bei Bedarf übersprungen werden (nein). ';
+$string['add_formmenu_provider'] = 'Anbieter als Auswahlbox (ja)';
+$string['formmenu_provider_help'] = '';
+$string['add_formmenu_courselanguage'] = 'Kurssprache als Auswahlbox (ja)';
+$string['formmenu_courselanguage_help'] = '';
+$string['add_formmenu_starttime'] = 'Startzeit als Auswahlbox (ja)';
+$string['formmenu_starttime_help'] = '';
+$string['minutes'] = "Minuten";
+$string['workload_in_min'] = "Zeiteinheit des Workloads";
+$string['workload_in_min_help'] = "Workload kann in Minuten oder Stunden angegeben werden";
+// END ADDED
