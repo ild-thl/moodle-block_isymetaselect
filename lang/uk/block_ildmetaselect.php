8d7beb5f0a9ebd9b9e1b2cb718be44bfb67fc0c7
Tom <thomas.muschal@th-luebeck.de>
2025-03-04 15:44:51 +0100

    Translations finished

--- lang/uk/block_ildmetaselect.php
+++ lang/uk/block_ildmetaselect.php
@@ -24,70 +24,69 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'ISy Course Overview';
+$string['pluginname'] = 'Огляд курсу ISy';
 
-$string['blocktitle'] = 'Kursübersicht';
+$string['blocktitle'] = 'Огляд курсу';
 
-$string['proctime_name'] = 'Bearbeitungszeit';
-$string['proctime_all'] = 'Alle Bearbeitungszeiten';
-$string['proctime_upto15'] = 'Bis 15 {$a}';
+$string['proctime_name'] = 'Час обробки';
+$string['proctime_all'] = 'Весь час обробки';
+$string['proctime_upto15'] = '< 15 {$a}';
 $string['proctime_between16and20'] = '16 - 20 {$a}';
 $string['proctime_between21and25'] = '21 - 25 {$a}';
-$string['proctime_morethan25'] = 'Über 25 {$a}';
+$string['proctime_morethan25'] = '> 25 {$a}';
 
-$string['starttime_name'] = 'Kursbeginn';
-$string['starttime_all'] = 'Alle Kursbeginne';
-$string['starttime_current'] = 'Aktuell laufend';
-$string['starttime_less2weeks'] = 'Innerhalb der nächsten 2 Wochen';
-$string['starttime_between3and4weeks'] = 'In 3-4 Wochen';
-$string['starttime_between5and6weeks'] = 'In 5-6 Wochen';
-$string['starttime_between7and8weeks'] = 'In 7-8 Wochen';
+$string['starttime_name'] = 'Початок курсу';
+$string['starttime_all'] = 'Всі курси починаються';
+$string['starttime_current'] = 'Наразі триває';
+$string['starttime_less2weeks'] = 'Протягом наступних 2 тижнів';
+$string['starttime_between3and4weeks'] = 'Через 3-4 тижні';
+$string['starttime_between5and6weeks'] = 'Через 5-6 тижні';
+$string['starttime_between7and8weeks'] = 'Через 7-8 тижні';
 
 $string['courselanguage'] = 'Мова курсу';
-$string['courselanguage_all'] = 'Alle Kurssprachen';
+$string['courselanguage_all'] = 'Всі мови курсу';
 
-$string['provider'] = 'Institution';
-$string['provider_all'] = 'Alle Institutionen';
+$string['provider'] = 'Установа';
+$string['provider_all'] = 'Всі установи';
 
 $string['subjectarea'] = 'Галузь знань';
-$string['subjectarea_all'] = 'Alle Wissensgebiete';
+$string['subjectarea_all'] = 'Всі галузі знань';
 
 $string['lecturer_detail'] = 'Автори / авторки';
-$string['provider_detail'] = 'Anbietende Hochschule';
+$string['provider_detail'] = 'Пропонований університет';
 $string['courselanguage_detail'] = 'Мова курсу';
 $string['subjectarea_detail'] = 'Галузь знань';
 $string['avgworkload_detail'] = 'Середнє навантаження';
-$string['starttime_detail'] = 'Kursbeginn';
+$string['starttime_detail'] = 'Початок курсу';
 
-$string['hours'] = 'Stunden';
-$string['free'] = 'Kostenfrei';
-$string['enrol'] = 'Einschreiben';
-$string['tocourse'] = 'zum Kurs';
+$string['hours'] = 'Години';
+$string['free'] = 'Безкоштовно';
+$string['enrol'] = 'Рекомендований лист';
+$string['tocourse'] = 'на курс';
 
-$string['metashare'] = 'teilen';
-$string['metatweet'] = 'tweeten';
+$string['metashare'] = 'Поділіться';
+$string['metatweet'] = 'твітнути';
 
-$string['noresultsfound'] = 'Keine Ergebnisse gefunden.';
+$string['noresultsfound'] = 'Не знайдено жодного результату';
 
 $string['h_awaits'] = 'Що очікує Вас на даному курсі?';
 $string['h_learn'] = 'Чого можна навчитися на цьому курсі?';
 $string['h_outline'] = 'Структура';
 $string['h_further_auth'] = 'Автори / авторки';
 $string['h_target_group'] = 'Цільова група';
-$string['h_confirmation'] = 'Teilnahmebestätigung';
+$string['h_confirmation'] = 'Підтвердження участі';
 $string['license'] = 'Ліцензія';
 
-$string['filter'] = 'Filtern';
-// ADDED tinjohn 20220920
-$string['add_detail_page'] = 'Kursdetailseite anzeigen';
-$string['add_detail_page_help'] = 'Die Kursdetailseite zeigt weitere Inhaltsangaben zum Kurs und stellt einen zusätzlichen Schritt bis zur Einschreibung dar. Dieser Schritt kann bei Bedarf übersprungen werden (nein). ';
-$string['add_formmenu_provider'] = 'Anbieter als Auswahlbox (ja)';
+$string['filter'] = 'Фільтр';
+
+$string['add_detail_page'] = 'Показати сторінку з деталями курсу';
+$string['add_detail_page_help'] = 'Сторінка деталей курсу містить додаткову інформацію про курс і є додатковим кроком на шляху до реєстрації. Цей крок можна пропустити, якщо потрібно (ні)';
+$string['add_formmenu_provider'] = 'Провайдер як поле для вибору (так)';
 $string['formmenu_provider_help'] = '';
-$string['add_formmenu_courselanguage'] = 'Kurssprache als Auswahlbox (ja)';
+$string['add_formmenu_courselanguage'] = 'Мова курсу як поле для вибору (так)';
 $string['formmenu_courselanguage_help'] = '';
-$string['add_formmenu_starttime'] = 'Startzeit als Auswahlbox (ja)';
+$string['add_formmenu_starttime'] = 'Час початку як поле для вибору (так)';
 $string['formmenu_starttime_help'] = '';
-$string['minutes'] = "Minuten";
-$string['workload_in_min'] = "Zeiteinheit des Workloads";
-$string['workload_in_min_help'] = "Workload kann in Minuten oder Stunden angegeben werden";
-// END ADDED
+$string['minutes'] = "хвилини";
+$string['workload_in_min'] = "Часова одиниця робочого навантаження";
+$string['workload_in_min_help'] = "Навантаження можна вказати в хвилинах або годинах";
